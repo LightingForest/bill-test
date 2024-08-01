f622ef613b4ddcc17710493d09c8532a4c073adb
Robin_ <3045351695@qq.com>
2024-08-01 20:36:04 +0800

    "tabbar切换功能实现"

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,21 +1,58 @@
-import {Outlet} from "react-router-dom";
-import {Button} from "antd-mobile";
-import {useDispatch} from "react-redux";
-import {useEffect} from "react";
+import { TabBar } from "antd-mobile"
+import { useEffect } from "react"
+import {Outlet, useNavigate} from "react-router-dom"
+import { useDispatch } from 'react-redux'
 import {getBillList} from "../../store/modules/billStore";
+import './index.scss'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+} from 'antd-mobile-icons'
 
-const Layout=()=>{
-    const dispatch=useDispatch()
+
+const tabs = [
+    {
+        key: '/month',
+        title: '月度账单',
+        icon: <BillOutline />,
+    },
+    {
+        key: '/new',
+        title: '记账',
+        icon: <AddCircleOutline />,
+    },
+    {
+        key: '/year',
+        title: '年度账单',
+        icon: <CalculatorOutline />,
+    },
+]
+
+const Layout = () => {
+    const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getBillList())
-    }, [dispatch]);
+    }, [dispatch])
+    const navigate=useNavigate()
+    const swithRoute=(path)=>{
+        console.log(path)
+        navigate(path)
+    }
     return (
-        <div>
-            <Outlet/>
-            我是layout
-        {/*    测试全局生效样式*/}
-            <Button color="primary">测试全局</Button>
+        <div className="layout">
+            <div className="container">
+                <Outlet />
+            </div>
+            <div className="footer">
+                <TabBar onChange={swithRoute}>
+                    {tabs.map(item => (
+                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    ))}
+                </TabBar>
+            </div>
         </div>
     )
 }
-export default Layout;
+
+export default Layout
