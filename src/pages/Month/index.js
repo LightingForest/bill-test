a1324cfeb040b385c08646c27a23848b8f66156f
Robin_ <3045351695@qq.com>
2024-08-01 21:12:22 +0800

    "月度账单切换弹窗功能"

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,4 +1,58 @@
-const Month=()=>{
-    return <div>我是month</div>
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+import {useState} from "react";
+import classNames from "classnames";
+
+const Month = () => {
+    const [dateVisible, setDateVisible] = useState(false)
+    const onConfirm=()=>{
+        setDateVisible(false)
+    }
+    return (
+        <div className="monthlyBill">
+            <NavBar className="nav" backArrow={false}>
+                月度收支
+            </NavBar>
+            <div className="content">
+                <div className="header">
+                    {/* 时间切换区域 */}
+                    <div className="date" onClick={()=>setDateVisible(true)}>
+            <span className="text">
+              2023 | 3月账单
+            </span>
+                        {/*根据当前弹框打开的状态控制expand类名是否存在*/}
+                        <span className={classNames('arrow',dateVisible && 'expand')}></span>
+                    </div>
+                    {/* 统计区域 */}
+                    <div className='twoLineOverview'>
+                        <div className="item">
+                            <span className="money">{100}</span>
+                            <span className="type">支出</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{200}</span>
+                            <span className="type">收入</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{200}</span>
+                            <span className="type">结余</span>
+                        </div>
+                    </div>
+                    {/* 时间选择器 */}
+                    <DatePicker
+                        className="kaDate"
+                        title="记账日期"
+                        precision="month"
+                        visible={dateVisible}
+                        onCancel={()=>setDateVisible(false)}
+                        onConfirm={onConfirm}
+                        onClose={()=>setDateVisible(false)}
+                        max={new Date()}
+                    />
+                </div>
+            </div>
+        </div >
+    )
 }
-export default Month;
+
+export default Month
