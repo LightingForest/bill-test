d5aabff6c1e12cae049118fefd2974ee3ab03769
Robin_ <3045351695@qq.com>
2024-08-01 21:22:02 +0800

    "月度账单切换时间显示"

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -2,11 +2,21 @@ import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
 import {useState} from "react";
 import classNames from "classnames";
+import dayjs from "dayjs";
 
 const Month = () => {
+    //控制弹窗打开和关闭
     const [dateVisible, setDateVisible] = useState(false)
-    const onConfirm=()=>{
+    //控制时间显示
+    const [currentDate, setCurrentDate] = useState(()=>{
+        return dayjs(new Date()).format('YYYY-MM')
+    })
+    const onConfirm=(date)=>{
         setDateVisible(false)
+        console.log(date)
+        const formatDate=dayjs(date).format('YYYY-MM')
+        setCurrentDate(formatDate)
+        //其他逻辑
     }
     return (
         <div className="monthlyBill">
@@ -18,7 +28,7 @@ const Month = () => {
                     {/* 时间切换区域 */}
                     <div className="date" onClick={()=>setDateVisible(true)}>
             <span className="text">
-              2023 | 3月账单
+              {currentDate+''}账单
             </span>
                         {/*根据当前弹框打开的状态控制expand类名是否存在*/}
                         <span className={classNames('arrow',dateVisible && 'expand')}></span>
