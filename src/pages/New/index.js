743ec586a871bd48792ab0436fc34da466bb12c7
Robin_ <3045351695@qq.com>
2024-08-02 20:44:13 +0800

    "新增功能实现"

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -5,11 +5,33 @@ import classNames from 'classnames'
 import { billListData } from '../../contants'
 import { useNavigate } from 'react-router-dom'
 import {useState} from "react";
+import {addBillList} from "../../store/modules/billStore";
+import {useDispatch} from "react-redux";
 
 const New = () => {
     const navigate = useNavigate()
     //准备控制收入支出的状态
     const [billType, setBillType] = useState('pay')
+
+    //收集金额
+    const [money, setMoney] = useState(0)
+    const moneyChange=(value)=>{
+        setMoney(value)
+    }
+    //收集账单类型
+    const [useFor, setUseFor] = useState('')
+    const dispatch=useDispatch()
+    //保存账单
+    const saveBill=()=>{
+        //收集表单数据
+        const data={
+            type:billType,
+            money:billType==='pay'?-money:+money,
+            date:new Date(),
+            useFor:useFor
+        }
+        dispatch(addBillList(data))
+    }
     return (
         <div className="keepAccounts">
             <NavBar className="nav" onBack={() => navigate(-1)}>
@@ -50,6 +72,8 @@ const New = () => {
                                 className="input"
                                 placeholder="0.00"
                                 type="number"
+                                value={money}
+                                onChange={moneyChange}
                             />
                             <span className="iconYuan">¥</span>
                         </div>
@@ -71,6 +95,7 @@ const New = () => {
                                                 ''
                                             )}
                                             key={item.type}
+                                            onClick={()=>setUseFor(item.type)}
 
                                         >
                                             <div className="icon">
@@ -87,7 +112,7 @@ const New = () => {
             </div>
 
             <div className="btns">
-                <Button className="btn save">
+                <Button className="btn save" onClick={saveBill}>
                     保 存
                 </Button>
             </div>
--- src/store/modules/billStore.js
+++ src/store/modules/billStore.js
@@ -12,11 +12,15 @@ const billStore=createSlice({
         //同步修改方法
         setBillList(state,action){
             state.billList=action.payload
+        },
+        //同步添加账单方法
+        addBill(state,action){
+            state.billList.push(action.payload)
         }
     }
 })
 //解构actionCreater函数
-const {setBillList} =billStore.actions
+const {setBillList,addBill} =billStore.actions
 //编写异步
 const getBillList=()=>{
     return async (dispatch) => {
@@ -27,7 +31,14 @@ const getBillList=()=>{
 
     }
 }
-export {getBillList}
+
+const addBillList=(data)=>{
+    return async (dispatch)=>{
+        const res = await axios.post('http://localhost:8888/ka',data)
+        dispatch(addBill(res.data))
+    }
+}
+export {getBillList,addBillList}
 
 //导出reducer
 const reducer=billStore.reducer
