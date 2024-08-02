52aefa89c2413b108f0039b867033b072bf26c68
Robin_ <3045351695@qq.com>
2024-08-02 19:57:35 +0800

    "账单图标组件封装"

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import './index.scss'
 import {useMemo, useState} from "react";
 import {billTypeToName} from "../../../../contants";
+import Icon from "../../../../components/Icon";
 
 const DailyBill = ({date,billList}) => {
     //计算单日统计
@@ -45,6 +46,8 @@ const DailyBill = ({date,billList}) => {
                 {billList.map(item => {
                     return (
                         <div className="bill" key={item.id}>
+                            {/*图标*/}
+                            <Icon type={item.useFor}/>
                             <div className="detail">
                                 <div className="billType">{billTypeToName[item.useFor]}</div>
                             </div>
